ea411947da74fdb934cc6bf0a00e8a4e941bc3cb
FunGhoul <kviraj37@gmail.com>
2018-04-01 20:39:51 +0530

    Noob commit (Added basic layout for help screen)

---
+++ app/src/main/java/com/dk/watercup/HelpActivity.java
@@ -0,0 +1,16 @@
+package com.dk.watercup;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        this.setTitle(R.string.Help);
+    }
+}
